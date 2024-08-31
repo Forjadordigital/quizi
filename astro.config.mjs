f36e6baf49d8d92704714b04567e8e4d6bcab430
Enrique Prosperi <laforjadigital.core@gmail.com>
2024-08-31 14:42:26 -0400

    Configuración de Cloudflare en la web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
   output: "server",
+  adapter: cloudflare()
 });

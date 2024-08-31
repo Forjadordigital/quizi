48859a7a49cb5ff9d0fe311fa479b62049f9ba2f
Enrique Prosperi <laforjadigital.core@gmail.com>
2024-08-31 16:31:33 -0400

    cambio nombres de las variables de entorno

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 
 export const getClient = () => {
   return createClient({
-    url: import.meta.env.PUBLIC_DATABASE_URL ?? '',
-    authToken: import.meta.env.DATABASE_AUTH_TOKEN ?? ''
+    url: import.meta.env.TURSO_DB_URL ?? '',
+    authToken: import.meta.env.TURSO_DB_AUTH_TOKEN ?? ''
   });
 };
